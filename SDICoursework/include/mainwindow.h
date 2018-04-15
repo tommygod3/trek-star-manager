56eb9c9565f2fd6c04bf49d96c50152eb3b96f39
Tyler <tyler.knowles@hotmail.co.uk>
2018-04-15 21:52:28 +0100

    started gui

--- SDICoursework/include/mainwindow.h
+++ SDICoursework/include/mainwindow.h
@@ -15,6 +15,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_toAddEditProjects_clicked();
+
+    void on_toBrowseProjects_clicked();
+
+    void on_toMaintenance_clicked();
+
+    void on_homeButton1_clicked();
+
+    void on_homeButton2_clicked();
+
+    void on_homeButton3_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
--- SDICoursework/src/mainwindow.cpp
+++ SDICoursework/src/mainwindow.cpp
@@ -12,3 +12,33 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_toAddEditProjects_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_toBrowseProjects_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_toMaintenance_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+void MainWindow::on_homeButton1_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_homeButton2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_homeButton3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
